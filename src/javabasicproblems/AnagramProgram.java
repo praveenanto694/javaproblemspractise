a142f162ee69218d9afe17f96c5f2bac39e65d33
Praveen P <praveenat694@gmail.com>
2025-07-15 12:42:03 +0530

    Revert "Made changes to the Anagram program"
    This reverts commit b65c86f098d815a7d226411828b03c670baa6b2e.

--- src/javabasicproblems/AnagramProgram.java
+++ src/javabasicproblems/AnagramProgram.java
@@ -1,24 +1,16 @@
 package javabasicproblems;
 
-import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class AnagramProgram {
 
 	public static void main(String[] args) 
 	{
 		String ss1 = "listen";
-		String ss2 = "silene";
+		String ss2 = "silent";
 		
-		char[] ss1array = ss1.toCharArray();
-		char[] ss2array = ss2.toCharArray();
 		
-		Arrays.sort(ss1array);
-		Arrays.sort(ss2array);
-		
-		if(Arrays.equals(ss1array, ss2array))
-			System.out.println("The given strings are anagram");
-		else
-			System.out.println("The given strings are not an anagram");
 		
 	}
 
