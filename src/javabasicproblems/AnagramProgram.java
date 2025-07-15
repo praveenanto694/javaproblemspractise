b65c86f098d815a7d226411828b03c670baa6b2e
Praveen P <praveenat694@gmail.com>
2025-07-15 12:16:05 +0530

    Made changes to the Anagram program

--- src/javabasicproblems/AnagramProgram.java
+++ src/javabasicproblems/AnagramProgram.java
@@ -1,16 +1,24 @@
 package javabasicproblems;
 
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.Arrays;
 
 public class AnagramProgram {
 
 	public static void main(String[] args) 
 	{
 		String ss1 = "listen";
-		String ss2 = "silent";
+		String ss2 = "silene";
 		
+		char[] ss1array = ss1.toCharArray();
+		char[] ss2array = ss2.toCharArray();
 		
+		Arrays.sort(ss1array);
+		Arrays.sort(ss2array);
+		
+		if(Arrays.equals(ss1array, ss2array))
+			System.out.println("The given strings are anagram");
+		else
+			System.out.println("The given strings are not an anagram");
 		
 	}
 
